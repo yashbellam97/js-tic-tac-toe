6fa78264d4357c9bd3daf83fcaf4c93015f1e05c
yashbellam97 <yashbellam97@gmail.com>
2020-07-09 12:43:56 -0400

    Add game completion logic

--- script.js
+++ script.js
@@ -10,14 +10,27 @@ const gameBoard = (() => {
     const addSymbol = (target, symbol) => {
         let position = target.id;
         if (!board[position]) {
-            gameBoard[position] = symbol;
+            board[position] = symbol;
             target.textContent = symbol;
             makeInvalid(target);
+            if(checkGameOver()) console.log("YES");
         }
     }
 
     const makeInvalid = (target) => {
         target.classList.remove("valid");
+    }
+
+    const checkGameOver = () => {
+        return (
+            (board[0] && board[1] && board[2] && (board[0] === board[1] && board[0] === board[2]))
+            || (board[3] && board[4] && board[5] && (board[3] === board[4] && board[3] === board[5]))
+            || (board[6] && board[7] && board[8] && (board[6] === board[7] && board[6] === board[8]))
+            || (board[0] && board[3] && board[6] && (board[0] === board[3] && board[0] === board[6]))
+            || (board[1] && board[4] && board[7] && (board[1] === board[4] && board[1] === board[7]))
+            || (board[2] && board[5] && board[8] && (board[2] === board[5] && board[2] === board[8]))
+            || (board[0] && board[4] && board[8] && (board[0] === board[4] && board[0] === board[8]))
+            || (board[2] && board[4] && board[6] && (board[2] === board[4] && board[2] === board[6])));
     }
 
     return {board, addSymbol};
