16e59e5a4139850e27c911cb5a9a953788616d54
yashbellam97 <yashbellam97@gmail.com>
2020-07-09 14:45:07 -0400

    Assign different symbols to both players

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-function playerFactory() {
-
-}
-
-const hello = "HE";
-
 const gameBoard = (() => {
-    const board = [];
+    let board = [];
+    let currentSymbol = "x";
 
-    const addSymbol = (target, symbol) => {
+    let playerOneDiv = document.querySelector("#player-1-div");
+    let playerTwoDiv = document.querySelector("#player-2-div");
+
+    const addSymbol = (target) => {
         let position = target.id;
         if (!board[position]) {
-            board[position] = symbol;
-            target.textContent = symbol;
+            board[position] = currentSymbol;
+            target.textContent = currentSymbol;
+            currentSymbol = currentSymbol == "x" ? "o" : "x";
+            if (currentSymbol == "x") {
+                playerOneDiv.classList.add("current");
+                playerTwoDiv.classList.remove("current");
+            } else if (currentSymbol == "o") {
+                playerOneDiv.classList.remove("current");
+                playerTwoDiv.classList.add("current");
+            }
             makeInvalid(target);
-            if(checkGameOver()) console.log("YES");
+            let isGameOver = checkGameOver();
+            if(isGameOver) clearBoard(isGameOver);
         }
     }
 
@@ -22,15 +29,37 @@ const gameBoard = (() => {
     }
 
     const checkGameOver = () => {
-        return (
-            (board[0] && board[1] && board[2] && (board[0] === board[1] && board[0] === board[2]))
-            || (board[3] && board[4] && board[5] && (board[3] === board[4] && board[3] === board[5]))
-            || (board[6] && board[7] && board[8] && (board[6] === board[7] && board[6] === board[8]))
-            || (board[0] && board[3] && board[6] && (board[0] === board[3] && board[0] === board[6]))
-            || (board[1] && board[4] && board[7] && (board[1] === board[4] && board[1] === board[7]))
-            || (board[2] && board[5] && board[8] && (board[2] === board[5] && board[2] === board[8]))
-            || (board[0] && board[4] && board[8] && (board[0] === board[4] && board[0] === board[8]))
-            || (board[2] && board[4] && board[6] && (board[2] === board[4] && board[2] === board[6])));
+        let result;
+        result = board[0] && board[1] && board[2];
+        if (result && (board[0] === board[1] && board[0] === board[2])) return result;
+        result = board[3] && board[4] && board[5];
+        if (result && (board[3] === board[4] && board[3] === board[5])) return result;
+        result = board[6] && board[7] && board[8];
+        if (result && (board[6] === board[7] && board[6] === board[8])) return result;
+        result = board[0] && board[3] && board[6];
+        if (result && (board[0] === board[3] && board[0] === board[6])) return result;
+        result = board[1] && board[4] && board[7];
+        if (result && (board[1] === board[4] && board[1] === board[7])) return result;
+        result = board[2] && board[5] && board[8];
+        if (result && (board[2] === board[5] && board[2] === board[8])) return result;
+        result = board[0] && board[4] && board[8];
+        if (result && (board[0] === board[4] && board[0] === board[8])) return result;
+        result = board[2] && board[4] && board[6];
+        if (result && (board[2] === board[4] && board[2] === board[6])) return result;
+        if (board[0] && board[1] && board[2] && board[3] && board[4] && board[5] && board[6] && board[7] && board[8]) return "tie";
+        return null;
+    }
+
+    const clearBoard = (result) => {
+        if (result === "x") alert("Player 1 wins");
+        else if (result === "o") alert("Player 2 wins");
+        else if (result === "tie") alert("It's a tie!");
+        console.log(result);
+        board = [];
+        currentSymbol = "x";
+        playerOneDiv.classList.add("current");
+        playerTwoDiv.classList.remove("current");
+        displayController.intitializeGrid();
     }
 
     return {board, addSymbol};
@@ -47,7 +76,7 @@ const displayController = (() => {
             itemDiv.id = "" + i;
             itemDiv.classList.add("item", "valid");
             itemDiv.addEventListener("click", (event) => {
-                gameBoard.addSymbol(event.target, "X")
+                gameBoard.addSymbol(event.target)
             })
             gameContainer.appendChild(itemDiv);
         }
